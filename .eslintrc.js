1db99b43212615b3120cf370510f85bccff17228
unknown <jdruhan.home@gmail.com>
2021-05-07 12:20:28 -0400

    Initial commit.

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+	"env": {
+		"browser": true,
+		"es2021": true
+	},
+	"extends": [
+		"eslint:recommended",
+		"plugin:vue/essential",
+		"plugin:@typescript-eslint/recommended"
+	],
+	"parserOptions": {
+		"ecmaVersion": 12,
+		"parser": "@typescript-eslint/parser",
+		"sourceType": "module"
+	},
+	"plugins": [
+		"vue",
+		"@typescript-eslint",
+		"eslint-plugin-tsdoc"
+	],
+	"rules": {
+		"tsdoc/syntax": "warn",
+		"indent": [
+			"error",
+			"tab"
+		],
+		"linebreak-style": [
+			"error",
+			"windows"
+		],
+		"quotes": [
+			"error",
+			"double"
+		],
+		"semi": [
+			"error",
+			"always"
+		]
+	}
+};
