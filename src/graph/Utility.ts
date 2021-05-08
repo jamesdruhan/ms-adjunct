be9fcb0ce08663159368c2204a6f3bdbfda31f87
unknown <jdruhan.home@gmail.com>
2021-05-08 02:19:01 -0400

    Added enum for versions.

---
+++ src/graph/Utility.ts
@@ -0,0 +1,21 @@
+/**
+ * @file Graph utility ENUM types.
+ * 
+ * @author James Druhan
+ * 
+ * @license MIT
+ */
+
+/**
+ * Microsoft Graph version. Value indicates the version portion of a Graph request URL.
+ * 
+ * @readonly
+ * 
+ * @enum { string }
+ */
+export enum Version
+{
+    CURRENT = "v1.0",
+    ONE     = "v1.0",
+    BETA    = "beta"
+}
