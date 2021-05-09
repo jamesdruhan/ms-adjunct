8b88ce25ea1b5b81875f40c006c8d6727c00035a
James Druhan <jdruhan.home@gmail.com>
2021-05-09 02:18:32 -0400

    Added custom and MSAL methods of configuration.

--- src/config/Configuration.ts
+++ src/config/Configuration.ts
@@ -6,13 +6,28 @@
  * @license MIT
  */
 
-import { Version, AuthType } from "../graph/Utility";
+import { GraphVersion, LoginType, LoginPersistance } from "../utility/Utility";
 import { Configuration as MSALConfig } from "@azure/msal-browser";
 
-export interface Configuration extends MSALConfig
+export interface Configuration
 {
-    authType?     : AuthType,
-    graphVersion? : Version,
-    appScope?     : Array <string>,
-    graphScope?   : Array <string>
+    loginType?        : LoginType;
+    graphVersion?     : GraphVersion;
+
+    appScope?         : Array <string>;
+    graphScope?       : Array <string>;
+
+    // MSAL PublicClientApplication.Configuration:AuthOptions
+    clientId?         : string;
+    authority?        : string;
+    onSignInUri?      : string;
+
+    // MSAL PublicClientApplication.Configuration:CacheLocation
+    loginPersistance? : LoginPersistance;
+    ieSupport?        : boolean;
+    https?            : boolean;
+
+    // MSAL PublicClientApplication.Configuration
+    // https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/configuration.md
+    msalConfig?       : MSALConfig;    
 }
